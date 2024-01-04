68c2787b7d875d0b53923cc3ec65b6ce2132f2f2
Miguel D. Salcedo <misalcedo@github.com>
2024-01-04 11:30:40 -0500

    Cleanup generated library contents.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,4 @@
-//! A Primary Copy Method to Support Highly-Available Distributed Systems
+//! A Primary Copy Method to Support Highly-Available Distributed Systems.
 
-///
 pub struct Node {
-
-}
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
