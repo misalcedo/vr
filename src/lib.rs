0fe41e6d5d3757d1e21fbd7d5378fd57a236b8d7
Miguel D. Salcedo <misalcedo@github.com>
2024-01-12 19:43:23 -0500

    Implement a very basic version of the protocol.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! A Primary Copy Method to Support Highly-Available Distributed Systems.
 
-use std::collections::{HashMap, HashSet};
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 use std::io;
 use std::net::SocketAddr;
 
@@ -58,8 +59,10 @@ pub struct Replica<Service> {
     /// This records for each client the number of its most recent request,
     /// plus, if the request has been executed, the result sent for that request.
     client_table: HashMap<u128, RequestState>,
-    /// The last committed log entry.
-    committed: usize
+    /// The last log entry committed.
+    committed: usize,
+    /// Log entries yet to be committed log entry.
+    commit_heap: BinaryHeap<Reverse<usize>>
 }
 
 impl<Service> Replica<Service>
@@ -75,14 +78,63 @@ where Service: FnMut(Vec<u8>) -> Vec<u8> {
             log: vec![],
             client_table: Default::default(),
             committed: 0,
+            commit_heap: BinaryHeap::new(),
         }
     }
 
     pub fn poll(&mut self) -> io::Result<()> {
         let is_primary = (self.view_number % self.configuration.len()) == self.index;
 
-        match self.communication.receive() {
-            Ok((from, Message::Request(request))) if is_primary => {
+        let result = match self.communication.receive() {
+            Ok((from, message)) => self.process(is_primary, from, message),
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => Ok(()),
+            Err(e) => Err(e)
+        };
+
+        // Perform up-call for committed operations in order.
+        if is_primary {
+            self.commit()?;
+        }
+
+        result
+    }
+
+    fn commit(&mut self) -> io::Result<()> {
+        while let Some(Reverse(candidate)) = self.commit_heap.pop() {
+            let next = self.committed + 1;
+
+            if candidate == next {
+                if let Some(request) = self.log.get(candidate - 1).as_ref() {
+                    let reply = Reply {
+                        v: self.view_number,
+                        s: request.s,
+                        x: (self.service)(request.op.clone()),
+                    };
+
+                    if let Some(state) = self.client_table.get_mut(&request.c) {
+                        if state.request == request.s {
+                            state.reply = Some(reply.clone());
+
+                            // TODO: handle partial failure in committing an operation.
+                            self.communication.send(state.address, Message::Reply(reply))?;
+                        }
+                    }
+
+                    self.committed += 1;
+                }
+            } else if candidate > next {
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn process(&mut self, is_primary: bool, from: SocketAddr, message: Message) -> io::Result<()> {
+        match message {
+            Message::Request(request) if is_primary => {
                 let state = self.client_table.entry(request.c)
                     .or_insert_with(|| { RequestState::new(from, request.s) });
 
@@ -103,7 +155,7 @@ where Service: FnMut(Vec<u8>) -> Vec<u8> {
                     }
                 }
             }
-            Ok((from, Message::Prepare(message))) if !is_primary => {
+            Message::Prepare(message) if !is_primary => {
                 // TODO: ensure the prepare is from the current view's primary.
 
                 let next = self.log.len() + 1;
@@ -126,8 +178,8 @@ where Service: FnMut(Vec<u8>) -> Vec<u8> {
                     Ok(())
                 }
             }
-            Ok((from, Message::PrepareOk(message))) if is_primary => {
-                match self.log.get(message.n) {
+            Message::PrepareOk(message) if is_primary => {
+                match self.log.get(message.n - 1) {
                     None => Ok(()),
                     Some(request) => {
                         let state = self.client_table.entry(request.c)
@@ -136,22 +188,21 @@ where Service: FnMut(Vec<u8>) -> Vec<u8> {
                         if state.request == request.s {
                             state.accepted.insert(message.i);
 
-                            // TODO: Update committed.
                             let sub_majority = (self.configuration.len() - 1) / 2;
 
-                            (self.service)(request.op.clone())
-                        } else {
-                            Ok(())
+                            if state.accepted.len() >= sub_majority && self.committed < message.n {
+                                self.commit_heap.push(Reverse(message.n));
+                            }
                         }
+                        Ok(())
                     }
                 }
             }
-            Ok((from, Message::Commit(message))) => {
+            Message::Commit(message) if !is_primary && message.v == self.view_number => {
+                self.committed = message.n;
                 Ok(())
             }
-            Ok(_) => Ok(()),
-            Err(e) if e.kind() == io::ErrorKind::WouldBlock => Ok(()),
-            Err(e) => Err(e)
+            _ => Ok(()),
         }
     }
 
