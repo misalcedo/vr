2e01a50f356ee2d69d2c70d485532257317f4ff1
Miguel D. Salcedo <misalcedo@github.com>
2024-02-11 11:17:48 -0500

    Implement group iter using vec to allow random access and reversing.

--- src/identifiers.rs
+++ src/identifiers.rs
@@ -1,4 +1,5 @@
 use crate::stamps::View;
+use std::cmp::Ordering;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub struct ReplicaIdentifier(GroupIdentifier, usize);
@@ -14,6 +15,12 @@ impl ReplicaIdentifier {
 
     pub fn sub_majority(&self) -> usize {
         self.0.sub_majority()
+    }
+}
+
+impl PartialOrd for ReplicaIdentifier {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.1.partial_cmp(&other.1).filter(|_| self.0 == other.0)
     }
 }
 
@@ -42,7 +49,7 @@ impl GroupIdentifier {
     pub fn replicas(&self, view: View) -> impl Iterator<Item = ReplicaIdentifier> + Clone {
         let primary = self.primary(view);
 
-        ReplicaIterator(*self, 0).filter(move |r| r != &primary)
+        self.clone().into_iter().filter(move |r| r != &primary)
     }
 
     pub fn sub_majority(&self) -> usize {
@@ -50,28 +57,15 @@ impl GroupIdentifier {
     }
 }
 
-#[derive(Clone)]
-pub struct ReplicaIterator(GroupIdentifier, usize);
-
-impl Iterator for ReplicaIterator {
-    type Item = ReplicaIdentifier;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.1 >= self.0 .1 {
-            None
-        } else {
-            self.1 += 1;
-            Some(ReplicaIdentifier(self.0, self.1 - 1))
-        }
-    }
-}
-
 impl IntoIterator for GroupIdentifier {
     type Item = ReplicaIdentifier;
-    type IntoIter = ReplicaIterator;
+    type IntoIter = std::vec::IntoIter<ReplicaIdentifier>;
 
     fn into_iter(self) -> Self::IntoIter {
-        ReplicaIterator(self, 0)
+        let replicas: Vec<ReplicaIdentifier> =
+            (0..self.1).map(|i| ReplicaIdentifier(self, i)).collect();
+
+        replicas.into_iter()
     }
 }
 
@@ -116,6 +110,21 @@ mod tests {
                 ReplicaIdentifier(group, 0),
                 ReplicaIdentifier(group, 1),
                 ReplicaIdentifier(group, 2)
+            ]
+        );
+    }
+
+    #[test]
+    fn reversed() {
+        let group = GroupIdentifier::new(3);
+        let replicas: Vec<ReplicaIdentifier> = group.into_iter().rev().collect();
+
+        assert_eq!(
+            replicas,
+            vec![
+                ReplicaIdentifier(group, 2),
+                ReplicaIdentifier(group, 1),
+                ReplicaIdentifier(group, 0)
             ]
         );
     }
